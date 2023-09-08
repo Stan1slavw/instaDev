5300072268430c20ccc82cff1ea19126e93e0f05
Stan1slav <stas1234713@gmail.com>
2023-09-08 19:42:52 +0300

    test commit

---
+++ src/main/java/com/insta_spam/insta_spam/InstaSpamApplication.java
@@ -0,0 +1,13 @@
+package com.insta_spam.insta_spam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstaSpamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InstaSpamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/insta_spam/insta_spam/InstaSpamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insta_spam.insta_spam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstaSpamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
